cf578e15dd232f8a400eb9a1127c764c4920b670
cmuirhead <cmuirhead@192.168.2.13>
2021-02-05 14:32:58 -0500

    Telemarketeur

---
+++ Conditions/src/Téléphone.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Téléphone {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Donée les 4 derniers chiffres de le numéro de téléphone");
+		
+		int row1 = sc.nextInt(1) + sc.nextInt(2) + sc.nextInt(3) + sc.nextInt(4);
+		
+		if (int1==8 && )
+			
+			 
+			System.out.println("Ignore");
+		else
+			System.out.println("Answer"");
+	}
+
+}
